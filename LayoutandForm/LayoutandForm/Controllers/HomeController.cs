7e27f9662393e67629378d3e323940af25d0ec6d
tanvir.ahmeed.aiub <tanvir.ahmeed.aiub@gmail.com>
2023-10-02 15:40:15 +0600

    Form Validation  and value

--- LayoutandForm/LayoutandForm/Controllers/HomeController.cs
+++ LayoutandForm/LayoutandForm/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using LayoutandForm.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -20,15 +21,30 @@ namespace LayoutandForm.Controllers
             return View();
         }
         [HttpPost]
-        public ActionResult Login(FormCollection fc) {
-            ViewBag.UName = fc["Username"];
-            ViewBag.Password = fc["Password"];
-            TempData["Msg"] = "Login Successfull";
+        //public ActionResult Login(FormCollection fc) {
+        //public ActionResult Login(string Username, string Password) {
+        public ActionResult Login(LoginModel login) {
+
+            //ViewBag.UName = fc["Username"];
+            //ViewBag.Password = fc["Password"];
+            //TempData["Msg"] = "Login Successfull";
             //validation
             //authenticate
             //return Redirect("https://www.aiub.edu");
             //return RedirectToAction("Index","Dashboard");
-            return View();
+            return View(login);
+        }
+
+        [HttpGet]
+        public ActionResult SignUp() { 
+            return View() ;
+        }
+        [HttpPost]
+        public ActionResult SignUp(SignUpModel singUp) {
+            if (ModelState.IsValid) {
+                return RedirectToAction("Index");  
+            }
+            return View(singUp);
         }
     }
 }
---
+++ LayoutandForm/LayoutandForm/Models/SignUpModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace LayoutandForm.Models
+{
+    public class SignUpModel
+    {
+        [Required(ErrorMessage ="Provide FUll Name")]
+        [StringLength(5,ErrorMessage ="Name should be length of 5")]
+        
+        public string Name { get; set; }
+        [Required]
+        public string Gender { get; set; }
+        [Required]
+        public string Profession { get; set; }
+        [Required]
+        public string[] Hobbies { get; set; }
+    }
+}
