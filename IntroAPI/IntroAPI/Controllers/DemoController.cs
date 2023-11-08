5eac51b8a61d420287f5e65d55b3bc0716a9ba45
tanvir.ahmeed.aiub <tanvir.ahmeed.aiub@gmail.com>
2023-11-08 14:42:40 +0600

    Intro API

---
+++ IntroAPI/IntroAPI/Controllers/DemoController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace IntroAPI.Controllers
+{
+    public class DemoController : ApiController
+    {
+        [HttpGet]
+        [Route("api/nums")]
+        public HttpResponseMessage GetNums() { 
+            var nums = new int[] {1,2,3};
+            return Request.CreateResponse(HttpStatusCode.OK, nums);
+        }
+        [HttpPost]
+        [Route("api/test/post")]
+        public HttpResponseMessage Post() { 
+            return Request.CreateResponse(HttpStatusCode.OK,"Post received");
+        }
+    }
+}
---
+++ IntroAPI/IntroAPI/Controllers/DepartmentController.cs
@@ -0,0 +1,73 @@
+﻿using IntroAPI.EF;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace IntroAPI.Controllers
+{
+    public class DepartmentController : ApiController
+    {
+        [HttpGet]
+        [Route("api/departments/all")]
+        public HttpResponseMessage GetAll() {
+            var db = new DemoF23_CEntities();
+            try
+            {
+                var data = db.Departments.ToList();
+                return Request.CreateResponse(HttpStatusCode.OK, data);
+            }
+            catch (Exception ex) { 
+                return Request.CreateResponse(HttpStatusCode.InternalServerError, ex.Message);
+            }
+        }
+        [HttpGet]
+        [Route("api/departments/{id}")]
+        public HttpResponseMessage Get(int id) {
+            var db = new DemoF23_CEntities();
+            try {
+                var data = db.Departments.Find(id);
+                if (data != null) {
+                    return Request.CreateResponse(HttpStatusCode.OK, data);
+                }
+                else 
+                    return Request.CreateResponse(HttpStatusCode.NotFound,new {Msg="No data found" });
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateResponse(HttpStatusCode.InternalServerError, ex.Message);
+            }
+        }
+        [HttpPost]
+        [Route("api/departments/create")]
+        public HttpResponseMessage Create(Department d) {
+            var db = new DemoF23_CEntities();
+            try {
+                db.Departments.Add(d);
+                db.SaveChanges();
+                return Request.CreateResponse(HttpStatusCode.Created, new { Msg = "Created" });
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateResponse(HttpStatusCode.InternalServerError, ex.Message);
+            }
+        }
+        [HttpPost]
+        [Route("api/departments/update")]
+        public HttpResponseMessage Update(Department d) {
+            var db = new DemoF23_CEntities();
+            try {
+                var dept = db.Departments.Find(d.Id);
+                db.Entry(dept).CurrentValues.SetValues(d);
+                db.SaveChanges();
+                return Request.CreateResponse(HttpStatusCode.OK, dept);
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateResponse(HttpStatusCode.InternalServerError, ex.Message);
+            }
+        }
+    }
+}
